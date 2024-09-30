5012569086721de284ce7b6e7583d6a45ddc4b75
Kratos8830 <umarsayyed9922@gmail.com>
2024-09-30 20:35:34 +0530

    Revert "attack animation done"
    This reverts commit 9ada8ed4f341bc0f65ed319695d28725c9622987.

--- SHOGUN/Assets/Scripts/PlayerAttack.cs
+++ SHOGUN/Assets/Scripts/PlayerAttack.cs
@@ -1,13 +1,11 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerAttack : MonoBehaviour
 {
     public Animator animator;
-    private Rigidbody2D rb;
+
     public Transform attackPoint;
     public float attackRange = 0.5f;
     public LayerMask enemyLayers;
@@ -15,16 +13,6 @@ public class PlayerAttack : MonoBehaviour
 
     public float attackRate = 2f;
     float nextAttackTime = 0f;
-    
-
-    public float dashAmount = 24f;                // Dash distance or speed
-    public float DashTime = 0.2f;                 // Duration of dash
-    public float DashCoolDown = 1f;               // Cooldown time before next dash
-    public float distanceBetweenImages;
-    private bool canDash = true;                  // Can the player dash
-    private bool isDashing;                       // Is the player currently dashing
-    private float originalGravity;                // Store original gravity for dash
-    private float lastImageXpos;
 
     void Start()
     {
@@ -38,63 +26,12 @@ public class PlayerAttack : MonoBehaviour
         {
 
 
-            if (Input.GetKeyDown(KeyCode.E))
+            if (Input.GetKeyDown(KeyCode.Mouse0))
             {
-                animator.SetTrigger("attack");
-                DashMechanics();
-                Attack();
-                nextAttackTime = Time.time + 1f / attackRate;
-            }
-
-            if (Input.GetKeyDown(KeyCode.Q))
-            {
-                animator.SetTrigger("attack2");
                 Attack();
                 nextAttackTime = Time.time + 1f / attackRate;
             }
         }
-    }
-
-    void DashMechanics()
-    {
-       
-            StartCoroutine(Dash());
-            //anim.SetBool("dash", true);
-
-            //After Image Shit
-            if (Mathf.Abs(transform.position.x - lastImageXpos) > distanceBetweenImages)
-            {
-                AI_Pool.Instance.GetFromPool();
-                lastImageXpos = transform.position.x;
-            }
-        
-    }
-
-    private IEnumerator Dash()
-    {
-        canDash = false;
-        isDashing = true;
-        originalGravity = rb.gravityScale;
-        rb.gravityScale = 0f;
-
-        // Determine the dash direction based on the player's facing direction
-        float dashDirection = GetComponent<Player>().isFacingRight ? 1f : -1f;
-        rb.velocity = new Vector2(dashDirection * dashAmount, 0f);
-
-        //After Image Shit
-        AI_Pool.Instance.GetFromPool();
-        lastImageXpos = transform.position.x;
-
-        yield return new WaitForSeconds(DashTime);
-        //anim.SetBool("dash", false);
-
-        // Stop the player's movement after the dash
-        rb.velocity = new Vector2(0f, rb.velocity.y);
-        rb.gravityScale = originalGravity;
-        isDashing = false;
-
-        yield return new WaitForSeconds(DashCoolDown);
-        canDash = true;
     }
 
     void Attack()
@@ -107,15 +44,13 @@ public class PlayerAttack : MonoBehaviour
 
         foreach(Collider2D enemy in hitEnemies)
         {
-           
+            animator.SetTrigger("mar");
             Debug.Log("hitting enemy");
             enemy.GetComponent<EnemyHealth>().TakeDamage(attackDamage);
         }
 
 
     }
-
-  
 
     private void OnDrawGizmos()
     {
